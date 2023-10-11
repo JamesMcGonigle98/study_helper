c428f2691c932c1a072252440b2361755452bb99
James McGonigle <jamesmcgonigle@kubrickgroup.com>
2023-10-11 14:51:19 +0100

    added sentence similarity

--- llm_chains.py
+++ llm_chains.py
@@ -20,27 +20,29 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
 from secret_key import openai_key, serpapi_key
 
 # %% --------------------------------------------------------------------------
 # Create the LLMs
 # -----------------------------------------------------------------------------
 llm = OpenAI(openai_api_key= openai_key)
-
-
+sentence_comparison_llm = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 # %% --------------------------------------------------------------------------
 # Prompt Templates
 # -----------------------------------------------------------------------------
 prompt_template_starting_qs = PromptTemplate(
     input_variables=['subject','qualification','subject_area'],
-    template = "I am currently studying for my {qualification} {subject} exam. I want to revise {subject_area}. Give me 5 questions of {qualification} standard to assess my knowledge. Return these questions as a comma seperated list please "
+    template = "I am currently studying for my {qualification} {subject} exam. I want to revise {subject_area}. Give me 1 question of {qualification} standard to assess my knowledge. Return these questions as a comma seperated list please "
 
 )
 
 prompt_template_starting_as = PromptTemplate(
-    input_variables=['5_questions','subject','qualification','subject_area'],
-    template = "For each of the following {5_questions}, write the answers. Return these answers as a comma seperated list "
+    input_variables=['question'],
+    template = "For each of the following {question}, write the answers. Return these answers as a comma seperated list "
 )
 
 
@@ -49,24 +51,41 @@ prompt_template_starting_as = PromptTemplate(
 # -----------------------------------------------------------------------------
 def initial_questions(subject, qualification ,subject_area):
 
-    initial_q_chain = LLMChain(llm=llm, prompt=prompt_template_starting_qs, output_key="5_questions")
+    initial_q_chain = LLMChain(llm=llm, prompt=prompt_template_starting_qs, output_key="question")
 
-    initial_a_chain = LLMChain(llm=llm, prompt=prompt_template_starting_as, output_key="5_answers")
+    initial_a_chain = LLMChain(llm=llm, prompt=prompt_template_starting_as, output_key="answer")
 
     chain = SequentialChain(
         chains = [initial_q_chain, initial_a_chain],
         input_variables=['subject','qualification','subject_area'],
-        output_variables=['5_questions','5_answers']
+        output_variables=['question','answer']
         )
     
-    response = chain({'sucject':subject,
+    response = chain({'subject':subject,
                       'qualification':qualification,
                       'subject_area':subject_area}
                       )
 
     return response
 
+# if __name__ == "__main__":
+#     print(initial_questions("Maths","GCSE","Algebra")
 
+
+def compare_sentences(sentence1, sentence2):
+
+    sentences = [sentence1, sentence2]
+
+    embeddings = sentence_comparison_llm.encode(sentences)
+
+    embedding1 = embeddings[0]
+    embedding2 = embeddings[1]
+
+    similarity_score = cosine_similarity([embedding1], [embedding2])[0][0]
+
+    return similarity_score
+
+    
 
 
 
--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import streamlit as st
+import llm_chains
 
 
 
@@ -33,16 +34,52 @@ subject = st.sidebar.selectbox(
 
 qualification = st.sidebar.selectbox(
     "Which exam are you revising for?",
-    ("GCSE","A-Level")
+    ("GCSE","A-Level"),
+    placeholder="GCSE"
 )
 
 subject_area = st.sidebar.text_input(
-    f"Which area of {subject} would you like to revise?"
+    f"Which area of {subject} would you like to revise?",
+    placeholder="Algebra"
 )
 
 search = st.sidebar.button("Help me study!")
 
-if search or subject_area:
+if search:
 
     st.write(f"You've chosen to study {subject}! Let's help you with {subject_area} so that you can pass your {qualification}!")
 
+    response = llm_chains.initial_questions(subject, qualification ,subject_area)
+
+    st.write("Let's start with some questions to gauge your knowledge:")
+    
+    question = response['question'].strip().split(",")
+    answer = response['answer'].strip().split(",")
+
+    question = question[0]
+    answer = answer[0]
+
+    st.write(f"{question}?")
+
+    st.header("Write your answer")
+
+    # Question 1 
+    user_answer_1 = st.text_input("Answer 1")
+    llm_answer_1 = answer[0]
+    check_q1 = st.button("Check my answer!")
+
+    if check_q1 or user_answer_1:
+        score = llm_chains.compare_sentences(user_answer_1, llm_answer_1)
+        
+        if score > 0.8:
+            st.write("This answer is perfect!")
+
+        elif 0.4 < score < 0.8:
+            st.write("This answer is good!")
+        
+        else:
+            st.write("Try again!")
+
+        st.write(f"The answer is {answer}")
+
+# %%
